1f3d739fa9d052d0d7d40caac1e9e166f62414a8
Sean <pixelpicosean@gmail.com>
2014-11-16 11:25:04 +0800

    Test drawing a circle from script

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -4,4 +4,8 @@ window.CanvasContext = __MURAL__.CanvasContext;
 
 var context = new CanvasContext();
 
-context.arc(32, 32, 32, 0, Math.PI, true);
+//context.strokeStyle = '#0ff';
+print('strokeStyle: ' + context.strokeStyle);
+print('globalAlpha: ' + context.globalAlpha);
+context.arc(32, 32, 32, 0, Math.PI * 2, true);
+context.stroke();
