c6f33b3a06d12a6857e1bc02fb7e8e33e11dba45
Sean <pixelpicosean@gmail.com>
2014-11-18 17:02:26 +0800

    Add test code to test save/restore functionality

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -2,3 +2,14 @@ window.canvas = document.createElement('canvas');
 
 var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d');
+
+context.strokeStyle = '##ff0';
+context.rect(0, 0, 100, 100);
+context.save();
+context.strokeStyle = '#f0f';
+context.rect(50, 50, 100, 100);
+context.restore();
+context.strokeStyle = '#0ff';
+context.rect(100, 100, 100, 100);
+
+context.stroke();
