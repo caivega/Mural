a416c9c2e878a1258b5e0b42d55d014b5ee3ff03
Sean <pixelpicosean@gmail.com>
2014-11-21 10:43:50 +0800

    Test of using rotate, save and rotate when drawing images

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,95 +7,40 @@ var canvas = document.getElementById('canvas'),
 context.canvas = canvas;
 
 // Functions
-drawGrid(context, '#d3d3d3', 10, 10);
-
-context.lineWidth = 0.5;
-// context.font = '32pt Ariel';
-var circles = [];
-
-for (var i = 0; i < 100; ++i) {
-   circles[i] = {
-        x: 100,
-        y: 100,
-        velocityX: 3*Math.random(),
-        velocityY: 3*Math.random(),
-        radius: 50*Math.random(),
-        color: 'rgba(' +
-            (Math.random()*255).toFixed(0) + ', ' +
-            (Math.random()*255).toFixed(0) + ', ' +
-            (Math.random()*255).toFixed(0) + ', 1.0)'
-    };
-}
-
-var paused = true;
-document.onmousedown = function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-
-    paused = !paused;
+var img = new Image();
+img.ready = false;
+img.onload = function() {
+    img.ready = true;
 };
+img.src = 'media/rect.png';
 
 function animate() {
-    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-    drawGrid(context, '#d3d3d3', 10, 10);
+    context.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (!paused) {
-        context.fillStyle = '#000';
-        context.fillText('Click to pause!', 32, 32);
+    if (img.ready) {
+        context.save();
+            context.rotate(Math.PI / 4);
 
-        circles.forEach(function(circle) {
             context.beginPath();
-            context.arc(circle.x, circle.y, circle.radius, 0, Math.PI*2, false);
-            context.fillStyle = circle.color;
-            context.fill();
-            adjustPosition(circle);
-        });
-    }
-    else {
-        context.fillStyle = '#000';
-        context.fillText('Click to start!', 32, 32);
+            context.moveTo(0, 0);
+            context.lineTo(64, 0);
+            context.lineTo(64, 64);
+            context.lineTo(0, 64);
+            context.closePath();
+            context.stroke();
+        context.restore();
+
+        context.drawImage(img, 0, 0);
+        context.drawImage(img, 300, 0);
+        context.drawImage(img, 0, 300);
+        context.drawImage(img, 300, 300);
     }
 
+    // Draw FPS
     context.fillStyle = '#f00';
     context.fillText(window.getAverageFPS().toFixed(2), 600, 12);
 
     requestAnimationFrame(animate);
-};
-
-function adjustPosition(circle) {
-    if (circle.x + circle.velocityX + circle.radius > context.canvas.width ||
-        circle.x + circle.velocityX - circle.radius < 0)
-    {
-        circle.velocityX = -circle.velocityX;
-    }
-
-    if (circle.y + circle.velocityY + circle.radius > context.canvas.height ||
-        circle.y + circle.velocityY - circle.radius  < 0)
-    {
-        circle.velocityY= -circle.velocityY;
-    }
-
-    circle.x += circle.velocityX;
-    circle.y += circle.velocityY;
-}
-
-function drawGrid(context, color, stepx, stepy) {
-    context.strokeStyle = color;
-    context.lineWidth = 0.5;
-
-   for (var i = stepx + 0.5; i < context.canvas.width; i += stepx) {
-        context.beginPath();
-        context.moveTo(i, 0);
-        context.lineTo(i, context.canvas.height);
-        context.stroke();
-    }
-
-    for (var i = stepy + 0.5; i < context.canvas.height; i += stepy) {
-        context.beginPath();
-        context.moveTo(0, i);
-        context.lineTo(context.canvas.width, i);
-        context.stroke();
-    }
 }
 
 animate();
