e6d9e82e4a658951e812b750767124189a0e954b
Sean <pixelpicosean@gmail.com>
2014-11-24 08:54:15 +0800

    Move the curve sample to another script file, and add tests for arcTo

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,83 +1,52 @@
 window.canvas = document.createElement('canvas');
 
-var canvas = document.getElementById('canvas');
+var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+
+context.lineWidth = 2;
 
 // Functions
-var ctx = canvas.getContext("2d");
-ctx.canvas = canvas;
+function roundedRect(cornerX, cornerY, width, height, cornerRadius) {
+   if (width > 0) context.moveTo(cornerX + cornerRadius, cornerY);
+   else           context.moveTo(cornerX - cornerRadius, cornerY);
 
-ctx.fillStyle = '#f0f';
+   context.arcTo(cornerX + width, cornerY,
+                 cornerX + width, cornerY + height,
+                 cornerRadius);
 
-window.pos_x = 0;
-window.pos_y = 0;
+   context.arcTo(cornerX + width, cornerY + height,
+                 cornerX, cornerY + height,
+                 cornerRadius);
 
-var w = canvas.width;
-var h = canvas.height;
-var w2 = w / 2;
-var h2 = h / 2;
+   context.arcTo(cornerX, cornerY + height,
+                 cornerX, cornerY,
+                 cornerRadius);
 
-var curves = [];
-for (var i = 0; i < 200; i++) {
-    curves.push({
-        current: Math.random() * 1000,
-        inc: Math.random() * 0.005 + 0.002,
-        color: '#' + (Math.random() * 0xFFFFFF << 0).toString(16) // Random color
-    });
+   if (width > 0) {
+      context.arcTo(cornerX, cornerY,
+                    cornerX + cornerRadius, cornerY,
+                    cornerRadius);
+   }
+   else {
+      context.arcTo(cornerX, cornerY,
+                    cornerX - cornerRadius, cornerY,
+                    cornerRadius);
+   }
 }
 
-var p = [0,0, 0,0, 0,0, 0,0];
-var animate = function() {
-    // Clear the screen - note that .globalAlpha is still honored,
-    // so this will only "darken" the screen a bit
+function drawRoundedRect(strokeStyle, fillStyle, cornerX, cornerY, width, height, cornerRadius) {
+   context.beginPath();
 
-    // ctx.globalCompositeOperation = 'source-over';
-    // ctx.clearRect(0, 0, w, h);
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, w, h);
+   roundedRect(cornerX, cornerY, width, height, cornerRadius);
 
-    // Use the additive blend mode to draw the bezier curves
-    // ctx.globalCompositeOperation = 'lighter';
+   context.strokeStyle = strokeStyle;
+   context.fillStyle = fillStyle;
 
-    // Calculate curve positions and draw
-    for (var i = 0; i < maxCurves; i++) {
-        var curve = curves[i];
-        curve.current += curve.inc;
-        for (var j = 0; j < p.length; j += 2) {
-            var a = Math.sin(curve.current * (j+3) * 373 * 0.0001);
-            var b = Math.sin(curve.current * (j+5) * 927 * 0.0002);
-            var c = Math.sin(curve.current * (j+5) * 573 * 0.0001);
-            p[j] = (a * a * b + c * a + b) * w * c + w2;
-            p[j + 1] = (a * b * b + c - a * b *c) * h2 + h2;
-        }
+   context.stroke();
+   context.fill();
+}
 
-        ctx.beginPath();
-        ctx.moveTo(p[0], p[1]);
-        ctx.bezierCurveTo( p[2], p[3], p[4], p[5], p[6], p[7] );
-        ctx.strokeStyle = curve.color;
-        ctx.stroke();
-    }
-    if (window.pos_x > 200) window.pos_x = 0;
-    if (window.pos_y > 200) window.pos_y = 0;
-    window.pos_x++;
-    window.pos_y++;
-
-    // Display FPS
-    ctx.fillStyle = '#f0f';
-    ctx.fillText(window.getAverageFPS().toFixed(2), 600, 12);
-
-    requestAnimationFrame(animate);
-};
-
-
-// The vertical touch position controls the number of curves;
-// horizontal controls the line width
-var maxCurves = 70;
-document.addEventListener('mousemove', function(ev) {
-    // ctx.lineWidth = (ev.touches[0].pageX / w) * 20;
-    // maxCurves = Math.floor((ev.touches[0].pageY/h) * curves.length);
-}, false);
-
-ctx.globalAlpha = 0.5;
-ctx.lineWidth = 2;
-
-animate();
+drawRoundedRect('#00f',  '#ff0',   50,  40,  100,  100, 10);
+drawRoundedRect('#808',  '#0f0',  275,  40, -100,  100, 20);
+drawRoundedRect('#f00',  '#fff',  300, 140,  100, -100, 30);
+drawRoundedRect('#fff',  '#00f',  525, 140, -100, -100, 40);
