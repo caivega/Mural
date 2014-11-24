ad362bdce23c8cf958380275d5eed43db0bc2772
Sean <pixelpicosean@gmail.com>
2014-11-25 07:57:28 +0800

    Test canvas.style width and height setting

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,50 +3,11 @@ window.canvas = document.createElement('canvas');
 var canvas = document.getElementById('canvas'),
     context = canvas.getContext('2d');
 
-context.lineWidth = 2;
-
 // Functions
-function roundedRect(cornerX, cornerY, width, height, cornerRadius) {
-   if (width > 0) context.moveTo(cornerX + cornerRadius, cornerY);
-   else           context.moveTo(cornerX - cornerRadius, cornerY);
+console.log('canvas.style.width: ' + canvas.style.width);
+console.log('canvas.style.height: ' + canvas.style.height);
 
-   context.arcTo(cornerX + width, cornerY,
-                 cornerX + width, cornerY + height,
-                 cornerRadius);
+canvas.style.width = canvas.style.height = '320px';
 
-   context.arcTo(cornerX + width, cornerY + height,
-                 cornerX, cornerY + height,
-                 cornerRadius);
-
-   context.arcTo(cornerX, cornerY + height,
-                 cornerX, cornerY,
-                 cornerRadius);
-
-   if (width > 0) {
-      context.arcTo(cornerX, cornerY,
-                    cornerX + cornerRadius, cornerY,
-                    cornerRadius);
-   }
-   else {
-      context.arcTo(cornerX, cornerY,
-                    cornerX - cornerRadius, cornerY,
-                    cornerRadius);
-   }
-}
-
-function drawRoundedRect(strokeStyle, fillStyle, cornerX, cornerY, width, height, cornerRadius) {
-   context.beginPath();
-
-   roundedRect(cornerX, cornerY, width, height, cornerRadius);
-
-   context.strokeStyle = strokeStyle;
-   context.fillStyle = fillStyle;
-
-   context.stroke();
-   context.fill();
-}
-
-drawRoundedRect('#00f',  '#ff0',   50,  40,  100,  100, 10);
-drawRoundedRect('#808',  '#0f0',  275,  40, -100,  100, 20);
-drawRoundedRect('#f00',  '#fff',  300, 140,  100, -100, 30);
-drawRoundedRect('#fff',  '#00f',  525, 140, -100, -100, 40);
+console.log('canvas.style.width: ' + canvas.style.width);
+console.log('canvas.style.height: ' + canvas.style.height);
