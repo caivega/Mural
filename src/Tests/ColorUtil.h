e4518969a60377b80b45a4f841cb807cb9ec24c8
Sean <pixelpicosean@gmail.com>
2014-11-27 09:04:33 +0800

    Fix compile issue

--- src/Tests/ColorUtil.h
+++ src/Tests/ColorUtil.h
@@ -13,19 +13,19 @@
 
 void ColorUtil()
 {
-    float aa = 0.0f, bb = 0.0f, cc = 0.0f, dd = 0.0f;
-    mural::stringToColorRGBA("#abc", aa, bb, cc, dd);
-    printf("color: %f, %f, %f, %f\n", aa, bb, cc, dd);
-    mural::stringToColorRGBA("#80ff00", aa, bb, cc, dd);
-    printf("color: %f, %f, %f, %f\n", aa, bb, cc, dd);
-    mural::stringToColorRGBA("rgb(0, 255, 0)", aa, bb, cc, dd);
-    printf("color: %f, %f, %f, %f\n", aa, bb, cc, dd);
-    mural::stringToColorRGBA("rgba(120, 60, 30, 0.6)", aa, bb, cc, dd);
-    printf("color: %f, %f, %f, %f\n", aa, bb, cc, dd);
-    mural::stringToColorRGBA("hsla(90, 100%, 50%, 1)", aa, bb, cc, dd);
-    printf("color: %f, %f, %f, %f\n", aa, bb, cc, dd);
-
-    printf("color string: %s", mural::colorRGBAToString(1.0f, 0.0f, 1.0f).c_str());
+//    float aa = 0.0f, bb = 0.0f, cc = 0.0f, dd = 0.0f;
+//    mural::stringToColorRGBA("#abc", aa, bb, cc, dd);
+//    printf("color: %f, %f, %f, %f\n", aa, bb, cc, dd);
+//    mural::stringToColorRGBA("#80ff00", aa, bb, cc, dd);
+//    printf("color: %f, %f, %f, %f\n", aa, bb, cc, dd);
+//    mural::stringToColorRGBA("rgb(0, 255, 0)", aa, bb, cc, dd);
+//    printf("color: %f, %f, %f, %f\n", aa, bb, cc, dd);
+//    mural::stringToColorRGBA("rgba(120, 60, 30, 0.6)", aa, bb, cc, dd);
+//    printf("color: %f, %f, %f, %f\n", aa, bb, cc, dd);
+//    mural::stringToColorRGBA("hsla(90, 100%, 50%, 1)", aa, bb, cc, dd);
+//    printf("color: %f, %f, %f, %f\n", aa, bb, cc, dd);
+//
+//    printf("color string: %s", mural::colorRGBAToString(1.0f, 0.0f, 1.0f).c_str());
 }
 
 #endif
