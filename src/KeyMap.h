ec44b4ab48b6ae61a692fa9b57981c06947fa9ad
Sean <pixelpicosean@gmail.com>
2014-12-01 09:16:19 +0800

    Better variable name

--- src/KeyMap.h
+++ src/KeyMap.h
@@ -12,21 +12,21 @@
 #include <map>
 #include <string>
 
-static std::map<int, int> KeyCodeMap;
-static std::map<int, std::string> CodeMap;
+static std::map<int, int> CiToKeyCodeMap;
+static std::map<int, std::string> CiToCodeMap;
 
 void initKeyCodeMap()
 {
     // Digit
     for (int i = 48; i < 48 + 10; ++i) {
-        KeyCodeMap[i] = i;
-        CodeMap[i] = "Digit"; CodeMap[i] += i;
+        CiToKeyCodeMap[i] = i;
+        CiToCodeMap[i] = "Digit"; CiToCodeMap[i] += i;
     }
 
     // a-z
     for (int i = 97; i < 97 + 26; ++i) {
-        KeyCodeMap[i] = i - 32;
-        CodeMap[i] = "Key"; CodeMap[i] += (i + ('A' - 'a'));
+        CiToKeyCodeMap[i] = i - 32;
+        CiToCodeMap[i] = "Key"; CiToCodeMap[i] += (i + ('A' - 'a'));
     }
 }
 
