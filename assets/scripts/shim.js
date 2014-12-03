9e6ff85b3009dba13b76674842bcc13df3a3831f
Sean <pixelpicosean@gmail.com>
2014-12-03 08:39:14 +0800

    Wrap shim code into an anonymous function (for perf)

--- assets/scripts/shim.js
+++ assets/scripts/shim.js
@@ -1,86 +1,90 @@
-__MURAL__.__defineSetter__ = function __defineSetter__(obj, key, set) {
-    Object.defineProperty(obj, key, {
-        enumerable: true, configurable: true,
-        set: set
-    });
-};
+(function(__MURAL__) { 'use strict';
 
-__MURAL__.__defineGetter__ = function __defineGetter__(obj, key, get) {
-    Object.defineProperty(obj, key, {
-        enumerable: true, configurable: true,
-        get: get
-    });
-};
-
-__MURAL__.__defineAccessor__ = function __defineAccessor__(obj, key, set, get) {
-    Object.defineProperty(obj, key, {
-        enumerable: true, configurable: true,
-        set: set, get: get
-    });
-};
-
-__MURAL__.Event = function(type) {
-    this.type = type;
-    this.cancelBubble = false;
-    this.cancelable = false;
-    this.target = null;
-
-    this.initEvent = function(type, bubbles, cancelable) {
-        this.type = type;
-        this.cancelBubble = bubbles;
-        this.cancelable = cancelable;
+    __MURAL__.__defineSetter__ = function __defineSetter__(obj, key, set) {
+        Object.defineProperty(obj, key, {
+            enumerable: true, configurable: true,
+            set: set
+        });
     };
 
-    this.preventDefault = function() {};
-    this.stopPropagation = function() {};
-};
-__MURAL__.eventMixin = (function() {
-    function createEvent(type) {
-        return new __MURAL__.Event(type);
-    }
-    function addEventListener(type, callback, useCapture) {
-        if (!this.events[type]) {
-            this.events[type] = [];
-        }
-        this.events[type].push(callback);
-    }
-    function removeEventListener(type, callback) {
-        var listeners = this.events[type];
-        if (!listeners) {
-            return;
-        }
+    __MURAL__.__defineGetter__ = function __defineGetter__(obj, key, get) {
+        Object.defineProperty(obj, key, {
+            enumerable: true, configurable: true,
+            get: get
+        });
+    };
 
-        for (var i = listeners.length; i--;) {
-            if (listeners[i] === callback) {
-                listeners.splice(i, 1);
+    __MURAL__.__defineAccessor__ = function __defineAccessor__(obj, key, set, get) {
+        Object.defineProperty(obj, key, {
+            enumerable: true, configurable: true,
+            set: set, get: get
+        });
+    };
+
+    __MURAL__.Event = function(type) {
+        this.type = type;
+        this.cancelBubble = false;
+        this.cancelable = false;
+        this.target = null;
+
+        this.initEvent = function(type, bubbles, cancelable) {
+            this.type = type;
+            this.cancelBubble = bubbles;
+            this.cancelable = cancelable;
+        };
+
+        this.preventDefault = function() {};
+        this.stopPropagation = function() {};
+    };
+    __MURAL__.eventMixin = (function() {
+        function createEvent(type) {
+            return new __MURAL__.Event(type);
+        }
+        function addEventListener(type, callback, useCapture) {
+            if (!this.events[type]) {
+                this.events[type] = [];
+            }
+            this.events[type].push(callback);
+        }
+        function removeEventListener(type, callback) {
+            var listeners = this.events[type];
+            if (!listeners) {
+                return;
+            }
+
+            for (var i = listeners.length; i--;) {
+                if (listeners[i] === callback) {
+                    listeners.splice(i, 1);
+                }
             }
         }
-    }
-    function dispatchEvent(event) {
-        var listeners = this.events[event.type];
-        // Call onevent method if exists
-        if (typeof this['on' + event.type] === 'function') {
-            this['on' + event.type](event);
+        function dispatchEvent(event) {
+            var listeners = this.events[event.type];
+            // Call onevent method if exists
+            if (typeof this['on' + event.type] === 'function') {
+                this['on' + event.type](event);
+            }
+            if (!listeners) {
+                return;
+            }
+
+            for (var i = 0; i < listeners.length; i++) {
+                listeners[i](event);
+            }
         }
-        if (!listeners) {
-            return;
-        }
 
-        for (var i = 0; i < listeners.length; i++) {
-            listeners[i](event);
-        }
-    }
+        return function() {
+            this.events = {};
+            this.createEvent = createEvent;
+            this.addEventListener = addEventListener;
+            this.removeEventListener = removeEventListener;
+            this.dispatchEvent = dispatchEvent;
 
-    return function() {
-        this.events = {};
-        this.createEvent = createEvent;
-        this.addEventListener = addEventListener;
-        this.removeEventListener = removeEventListener;
-        this.dispatchEvent = dispatchEvent;
+            return this;
+        };
+    })();
 
-        return this;
-    };
-})();
+    // Array to save CanvasContext instances from GC
+    __MURAL__.contextPool = [];
 
-// Array to save CanvasContext instances from GC
-__MURAL__.contextPool = [];
+})(__MURAL__);
