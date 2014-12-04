97186455dda29f6bf8bdd0c5f1da285511d9c2ba
Sean <pixelpicosean@gmail.com>
2014-12-04 23:02:40 +0800

    Add a simple drawing example from MDN

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,13 +1,24 @@
 (function() { 'use strict';
 
-    var canvas = document.getElementById('canvas'),
-        context = canvas.getContext('2d');
+    function draw() {
+      var ctx = document.getElementById('canvas').getContext('2d');
+      ctx.translate(75,75);
 
-    print('default font: ' + context.font);
-    context.font = '32pt Arial';
-    print('default font: ' + context.font);
+      for (var i=1;i<6;i++){ // Loop through rings (from inside to out)
+        ctx.save();
+        ctx.fillStyle = 'rgb('+(51*i)+','+(255-51*i)+',255)';
 
-    context.fillStyle = '#808';
-    context.fillText('Font: Arial', 200, 200);
+        for (var j=0;j<i*6;j++){ // draw individual dots
+          ctx.rotate(Math.PI*2/(i*6));
+          ctx.beginPath();
+          ctx.arc(0,i*12.5,5,0,Math.PI*2,true);
+          ctx.fill();
+        }
+
+        ctx.restore();
+      }
+    }
+
+    draw();
 
 })();
