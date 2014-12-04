f8443556c566a98f1731316fab0f6cf83b8f50cc
Sean <pixelpicosean@gmail.com>
2014-12-04 08:46:22 +0800

    Simple localStorage test code

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,23 +1,8 @@
 (function() { 'use strict';
 
-    window.canvas = document.createElement('canvas');
-
-    var canvas = document.getElementById('canvas'),
-        context = canvas.getContext('2d');
-
-    var lastCode = 'NONE';
-    document.addEventListener('keydown', function(e) {
-        lastCode = e.code;
-    });
-
-    context.fillStyle = '#808';
-    (function animate() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.fillText(getAverageFPS().toFixed(2), canvas.width - 80, 20);
-
-        context.fillText('KEY: ' + lastCode, canvas.width * 0.5 - 60, canvas.height * 0.5);
-
-        requestAnimationFrame(animate);
-    })();
+    localStorage['aaa'] = '000';
+    localStorage.setItem('bbb', '111');
+    print('getItem "aaa" = ' + localStorage.getItem('aaa'));
+    print('getItem "bbb" = ' + localStorage.getItem('bbb'));
 
 })();
